01dadd4000f9be16f2e94f53842de7be32ca8b2b
Sergey Synkov <adm@it-fix.kz>
2020-04-27 22:09:13 +0600

    html, css, js

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+	'use strict';
+
+	paper.install(window);
+	paper.setup(document.getElementById('mainCanvas'));
+
+	var tool = new Tool();
+	tool.onMouseDown = function (event) {
+		var c = Shape.Circle(200, 200, 80);
+		c.fillColor = 'Blасk ';
+		var text = new PointText(200, 200);
+		text.justification = 'center';
+		text.fillColor = 'white';
+		text.fontSize = 20;
+		text.content = 'hello world';
+	}
+	paper.view.draw();
+
+});
